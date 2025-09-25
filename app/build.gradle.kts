b06352776d271c52c8ac2e2c13a04a4ca36ebb38
tuilatin <nguyentin1763@gmail.com>
2025-09-25 20:12:10 +0700

    push man hinh set payment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
